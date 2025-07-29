b76c589c42cc48e5665ee6153389d3b69e506148
yazid12345678910 <yazidcm2020@gmail.com>
2025-07-29 07:49:08 -0600

    Update consultar_modelo.php

--- consultar_modelo.php
+++ consultar_modelo.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "usuario", "contraseña", "mi_base_datos");
+$conexion = new mysqli("localhost", "root", "", "lam");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
